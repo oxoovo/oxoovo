35a8e3ffdb4fab853df8b698390ce1dc17661455
oxoovo <102413837+oxoovo@users.noreply.github.com>
2022-06-11 18:40:56 +0200

    Update visitor.py

--- visitor.py
+++ visitor.py
@@ -3,7 +3,10 @@ import os
 
 repo = Github(os.environ['GITHUB_TOKEN']).get_repo(os.environ['GITHUB_REPOSITORY'])
 issue = repo.get_issue(number=int(os.environ['ISSUE_NUMBER']))
-issue_author = '* ' + issue.user.login
+issue_author2 = '* ' + issue.user.login
+issue_author = '@' + issue.user.login
+repo_owner = '@' + os.environ['REPOSITORY_OWNER']
+issue.create_comment(settings['comments']['big_error'].format(author=issue_author, repo_owner=repo_owner))
 
 a = open("README.md", "a")
 a.write(issue_author)
