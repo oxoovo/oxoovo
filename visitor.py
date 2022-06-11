cf03be14936085cd9e290929bae0eca3a1223225
oxoovo <102413837+oxoovo@users.noreply.github.com>
2022-06-11 18:44:09 +0200

    Update visitor.py

--- visitor.py
+++ visitor.py
@@ -6,7 +6,7 @@ issue = repo.get_issue(number=int(os.environ['ISSUE_NUMBER']))
 issue_author2 = '* ' + issue.user.login
 issue_author = '@' + issue.user.login
 repo_owner = '@' + os.environ['REPOSITORY_OWNER']
-issue.create_comment(settings['comments']['big_error'].format(author=issue_author, repo_owner=repo_owner))
+issue.create_comment("Thank you for adding your name 😀")
 
 a = open("README.md", "a")
 a.write(issue_author)
